bd00f859337ba96928cf640cfc737064c2654d05
Tegger <42648199+Tegger@users.noreply.github.com>
2018-09-03 19:12:54 -0500

    Add files via upload

---
+++ variables/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte byteTest = 1;
+        short shortTest = 2;
+        int intTest = 3;
+        long longTest = 4;
+        float floatTest = 5;
+        double doubleTest = 6;
+        boolean boolTest = true;
+        char charTest = 'a';
+
+        System.out.println(byteTest + " " + shortTest + " " + intTest + " " + longTest + " " +
+                floatTest + " " + doubleTest + " " + boolTest + " " +charTest);
+
+
+
+
+    }
+}
